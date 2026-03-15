1c4652cdd652647f8b743a7938b90d38bc6376e0
Denis Ariel Curipoma Morocho <DACURMOR@upv.edu.es>
2026-03-15 03:03:22 +0100

    prueba

--- src/main/java/com/votify/dto/EventDto.java
+++ src/main/java/com/votify/dto/EventDto.java
@@ -14,7 +14,6 @@ public class EventDto {
 
     public EventDto(Long id, String name, Date timeInitial, Date timeFinal) {
         //prueba de comentario
-
         this.id = id;
         this.name = name;
         this.timeInitial = timeInitial;
